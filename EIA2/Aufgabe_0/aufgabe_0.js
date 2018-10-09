e309be0c48c9122f73112f7fbadc877c5fd378b2
Chris <Chris@LAPTOP-T2ITAE3Q.eduroam.hs-furtwangen.de>
2018-10-09 16:35:25 +0200

    changes

--- EIA2/Aufgabe_0/aufgabe_0.js
+++ EIA2/Aufgabe_0/aufgabe_0.js
@@ -2,10 +2,10 @@ var i = "";
 function main() {
     var i = prompt("Wie hei�t du?");
     var node = document.getElementById("content");
-    node.innerHTML += "Moin";
+    node.innerHTML += "Moin ";
     node.innerHTML += i;
     node.innerHTML += ",  immer sch�n Cremig bleiben";
-    console.log("Moin", i, ", sei gegr��t");
+    console.log("Moin ", i, ", sei gegr��t");
 }
 document.addEventListener('DOMContentLoaded', main);
 //# sourceMappingURL=aufgabe_0.js.map
--- EIA2/Aufgabe_0/aufgabe_0.ts
+++ EIA2/Aufgabe_0/aufgabe_0.ts
@@ -3,10 +3,10 @@ var i : string ="";
     function main() {
         var i = prompt("Wie heißt du?");
         var node : any = document.getElementById("content");
-        node.innerHTML += "Moin";
+        node.innerHTML += "Moin ";
         node.innerHTML += i;
         node.innerHTML += ",  immer schön Cremig bleiben";
-        console.log("Moin",i,", sei gegrüßt");
+        console.log("Moin ",i,", sei gegrüßt");
         }
     
     document.addEventListener('DOMContentLoaded',main);
